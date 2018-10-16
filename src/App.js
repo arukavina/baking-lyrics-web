849b4f6031f773d67b34f87e6eea5c88ebee0f8d
Aquiles Mariani <aquilesmariani@gmail.com>
2018-10-16 14:08:30 -0300

    google analytics integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import ReactGA from 'react-ga'
 import configureStore from './store'
 
 const store = configureStore()
 class App extends Component {
+  initializeReactGA = () => {
+    ReactGA.initialize('UA-118412070-1');
+    ReactGA.pageview('/');
+  }
+
   render() {
     const { children } = this.props
     return (
